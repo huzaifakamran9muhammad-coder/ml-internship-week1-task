3cd2a0191db588b71bc325d6f4463da23fa308fe
huzaifakamran9muhammad-coder <huzaifa.kamran9muhammad@gmail.com>
2026-06-09 23:29:27 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("ML Internship Week 1")
+st.write("Python Basics Assignment")
+st.write("Topics Covered:")
+st.write("- Variables")
+st.write("- Data Types")
+st.write("- Conditions")
+st.write("- Loops")
+st.success("Week 1 Assignment Completed!")
